64137f8208c7b41d56acd1c3de21a1b766382076
harsh <harshrana2101@gmail.com>
2020-05-28 14:06:33 -0400

    version1.1

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        double miles = SpeedConverter.toMilesPerHour(20);
+        System.out.println("Miles = " + miles);
+        SpeedConverter.printCoversion(10.5);
+
+
+    }
+}
